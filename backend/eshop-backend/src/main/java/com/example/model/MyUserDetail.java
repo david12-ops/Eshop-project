501d192e05ba9de5b4ebc0700c2533cc250d47b4
david12-ops <77929483+david12-ops@users.noreply.github.com>
2026-05-19 14:15:48 +0200

    update

---
+++ backend/eshop-backend/src/main/java/com/example/model/MyUserDetail.java
@@ -0,0 +1,32 @@
+package com.example.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetail implements UserDetails {
+
+    private AppUser user;
+
+    public MyUserDetail(AppUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ " + user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
